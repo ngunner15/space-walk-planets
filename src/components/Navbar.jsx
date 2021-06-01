f9a3b43132e89d662e67210ef52066be60170e88
Nisarg Vadgama <nisargvv@gmail.com>
2021-06-01 17:34:47 -0400

    Refractor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,19 +7,17 @@ export default function Navigation(props) {
   return (
     <Navbar className="navbar" expand="lg">
       <Navbar.Brand className="navbar-brand">SpaceWalk</Navbar.Brand>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mx-auto">
-          <Nav.Link eventKey="1" href="/mercury">Mercury</Nav.Link>
-          <Nav.Link eventKey="2" href="/venus">Venus</Nav.Link>
-          <Nav.Link eventKey="3" href="/">Earth</Nav.Link>
-          <Nav.Link eventKey="4" href="/mars">Mars</Nav.Link>
-          <Nav.Link eventKey="5" href="/jupiter">Jupiter</Nav.Link>
-          <Nav.Link eventKey="6" href="/saturn">Saturn</Nav.Link>
-          <Nav.Link eventKey="7" href="/uranus">Uranus</Nav.Link>
-          <Nav.Link eventKey="8" href="/neptune">Neptune</Nav.Link>
-          <Nav.Link eventKey="9" href="/pluto">Pluto</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+      <Nav className="mx-auto navbar-links">
+        <Nav.Link eventKey="1" href="/mercury">Mercury</Nav.Link>
+        <Nav.Link eventKey="2" href="/venus">Venus</Nav.Link>
+        <Nav.Link eventKey="3" href="/">Earth</Nav.Link>
+        <Nav.Link eventKey="4" href="/mars">Mars</Nav.Link>
+        <Nav.Link eventKey="5" href="/jupiter">Jupiter</Nav.Link>
+        <Nav.Link eventKey="6" href="/saturn">Saturn</Nav.Link>
+        <Nav.Link eventKey="7" href="/uranus">Uranus</Nav.Link>
+        <Nav.Link eventKey="8" href="/neptune">Neptune</Nav.Link>
+        <Nav.Link eventKey="9" href="/pluto">Pluto</Nav.Link>
+      </Nav>
     </Navbar>
   )
 }
